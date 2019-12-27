400decf2bd2a7d70f8db906addfd1af3799da258
shynipv <57441362+shynipv@users.noreply.github.com>
2019-12-27 12:48:22 +0530

    Add files via upload

---
+++ form/content.php
@@ -0,0 +1,16 @@
+<div class="blog-post">
+  <h2 class="blog-post-title">schedule</h2>
+
+<table border="4">
+  <form method="POST">
+    <p><label for="username">DAYS</label>
+
+    </p>
+    <p><label for="password">ACTIVITIES</label>
+
+
+  </form>
+</table>
+  <hr />
+
+</div>
